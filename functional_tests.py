bf45a02c283dc45e61ea8b6908268121577be117
Alberto Medina <amedina@twitter.com>
2015-02-17 09:29:53 -0800

    Add redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool To-Do App and she goes to the website
 		# to check it out
@@ -36,10 +41,8 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter the page updates, and now the page lists
 		# "1. Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		# self.assertTrue(
 		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
 		# 		"New to-do item does not appear on list -- its text was:\n%s" % (
@@ -48,15 +51,19 @@ class NewVisitorTest(unittest.TestCase):
 
 		# There is still a text box inviting her to add another to-do
 		# She enters "Use peacock feathers to make a fly"
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now it shows both items
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# She visits that URL - her to-do list is still there.
-
+		self.fail('Finish the test!')
 
 if __name__ == "__main__":
 	unittest.main(warnings='ignore')
--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
 	text = models.TextField(default='')
+
+	def str(self):
+		print('Item: {0}'.format(self.text))
