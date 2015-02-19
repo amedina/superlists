a24d5231a8f9ec16e529c0525dc4b5d35472a1f5
Alberto Medina <amedina@twitter.com>
2015-02-19 13:30:58 -0800

    Add model component so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+class List(models.Model):
+	pass
+	#id = models.PositiveIntegerField(primary_key=True)
+
 # Create your models here.
 class Item(models.Model):
 	text = models.TextField(default='')
+	list = models.ForeignKey(List, default=None)
 
 	def str(self):
 		print('Item: {0}'.format(self.text))
